76128dc153abac519b16b741b57d08c2f798bc12
baffmann <benjamin.beisel@gmail.com>
2019-10-06 17:36:46 +0200

    some ajax

--- web/js/dchs.js
+++ web/js/dchs.js
@@ -1,7 +1,50 @@
-$(document).ready(function () {
-    $('.header').height($(window).height());
+//$('.header').height($(window).height());
+$(document).ready(function() {
+    $.get("api/player", function(data) {
+        $(".playerlist").append("<p>Test</p>");
+    });
 })
 
-$(document).ready(function () {
-    $('.header').height($(window).height());
-})
+$("#neuerSpieler").click(function(e) {
+    //e.preventDefault();
+    //$(".playerlist").append("<p>Test</p>");
+    /*"last-name": $('#last-name').val()*/
+    var person = {
+        name: "Horst",
+        points: 501
+    };
+    $.ajax({
+        type: "POST",
+        url: "/api/player",
+        data: JSON.stringify(person),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(result) {
+            var returnedData = JSON.parse(result);
+            //returnedData.name
+            content = playerbuttonStart;
+            content += returnedData.name;
+            content += playerbuttonEnd;
+            $(content).appendTo('.playerlist');
+            $('.playerlist').append(createPlayerButton());
+        },
+        error: function(result) {
+            alert('Spieler konnte nicht angelegt werden.');
+        }
+    });
+});
+
+var playerbuttonStart = '<div class="row"><div class="col-sm-3"><button type="button" class="btn btn-primary btn-lg btn-block">';
+var playerbuttonEnd = '</button></div></div>';
+
+$(document).ready(function() {
+    $('.playerlist').append(playerbutton);
+});
+
+function createPlayerButton() {
+    return $('<button/>', {
+        text: 'Refresh Data',
+        id: 'btn_refresh',
+        click: ClickRefresh
+    });
+}
