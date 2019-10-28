a84cc289e29430036a5b16369d60c0079d4531bd
baffmann <benjamin.beisel@gmail.com>
2019-10-28 20:28:17 +0100

    createPlayerButton

--- web/js/dchs.js
+++ web/js/dchs.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     $.get("api/player", function(data) {
-        $(".playerlist").append("<p>Test</p>");
+        //for each player createPlayerButton
+        $.each(data, function(index){
+            createPlayerButton(data[index].name);
+        });
     });
 })
 
@@ -22,12 +25,7 @@ $("#neuerSpieler").click(function(e) {
         dataType: "json",
         success: function(result) {
             var returnedData = result;
-            //returnedData.name
-            content = playerbuttonStart;
-            content += returnedData.name;
-            content += playerbuttonEnd;
-            $(content).appendTo('#playerlist');
-            $('.playerlist').append(createPlayerButton());
+            createPlayerButton(returnedData.name);
         },
         error: function(result) {
             alert('Spieler konnte nicht angelegt werden.');
@@ -37,3 +35,11 @@ $("#neuerSpieler").click(function(e) {
 
 let playerbuttonStart = '<div class="col-lg-2" id="spielerbutton"><button type="button" class="btn btn-primary btn-lg btn-block">';
 let playerbuttonEnd = '</button></div>';
+
+function createPlayerButton(name){
+    console.log("createPlayerButton");
+    content = playerbuttonStart;
+    content += name;
+    content += playerbuttonEnd; 
+    $('#playerlist').append(content);
+}
